9f11100cede26fd3f23fe2c6d3727be8e10d48e4
S M Jamal Uddin al Gilani <136183664+Jamal-al-Gillani@users.noreply.github.com>
2024-06-24 19:26:49 +0000

    new program related to loops

--- practice1.py
+++ practice1.py
@@ -1,3 +1,16 @@
-n = int(input("Enter the number to find its table : "))
-for i in range (1,11):
-    print(f"{n} X {i} = {n*i}")
+# n = int(input("Enter the number to find its table : "))
+# for i in range (1,11):
+#     print(f"{n} X {i} = {n*i}")
+
+
+# n= 5
+# for i in range (1,n+1):
+#     print(" "* (n-i), end ="")
+#     print("*"* (2*i-1),end = "" )
+#     print("")
+
+n= 1
+for i in range (5,n-1):
+    print(" "* (n+i), end ="")
+    print("*"* (2*i+1),end = "" )
+    print("")
